13932379772b1f137035ee90284f734dce28ef68
brysonwaisi <brysonnyamwange@gmail.com>
2022-12-23 00:51:46 +0300

    Pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 '''A module for working with Pascal's triangle.
 '''
-def pascal_triangle(n):
-  res = [[1]]
 
-  if type(n) is not int or n<= 0:
+
+def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    res = []
+    if type(n) is not int or n <= 0:
+        return res
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            elif i > 0 and j > 0:
+                row.append(res[i - 1][j - 1] + res[i - 1][j])
+        res.append(row)
     return res
-    
-  for i in range(n - 1):
-    temp = [0] + res[-1] + [0]
-    row = []
-    for j in range(len(res[-1]) + 1):
-      row.append(temp[j] + temp[j+1])
-    res.append(row)
-  return res
 
 # Time Complexity O(n^2)
 # Space Complexity 0(n^2)

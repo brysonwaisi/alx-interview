bae1efdf9a486d0c48c8bda85feffc1c857d9d25
brysonwaisi <brysonnyamwange@gmail.com>
2022-12-23 00:37:04 +0300

    Pascal's Triangle Implementation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,2 +1,13 @@
 def pascal_triangle(n):
-  pass
+  res = [[1]]
+
+  for i in range(n - 1):
+    temp = [0] + res[-1] + [0]
+    row = []
+    for j in range(len(res[-1]) + 1):
+      row.append(temp[j] + temp[j+1])
+    res.append(row)
+  return res
+
+# Time Complexity O(n^2)
+# Space Complexity 0(n^2)

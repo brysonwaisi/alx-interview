9b862590ab86e7d785449fdc1df930869d648833
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-21 17:32:16 +0300

    Make change greedy algorithm approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-'''We implement a dynamic programming approach'''
+'''We implement a greedy approach'''
 
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    coins.sort()
-    dp = [float('inf')] * (total+1)
-    dp[0] = 0
-    for c in coins:
-        last_c = -1
-        for i in range(c, total+1):
-            if dp[i-c] != float('inf') and last_c < dp[i-c]:
-                dp[i] = min(dp[i], dp[i-c]+1)
-                last_c = dp[i-c]
-    return dp[total] if dp[total] < float('inf') else -1
+    coins.sort(reverse=True)
+    coin_count = 0
+    for coin in coins:
+        if total >= coin:
+            coin_count += total // coin
+            total %= coin
+        if total == 0:
+            return coin_count
+    return -1

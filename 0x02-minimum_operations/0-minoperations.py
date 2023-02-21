6ec7b52b10d6c45ce5b59417d3c014d3fe81cfaf
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-21 17:59:01 +0300

    Minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,29 +7,12 @@ def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
     '''
-    if not isinstance(n, int):
+    if n <= 1:
         return 0
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    # print('H', end='')
-    while done < n:
-        if clipboard == 0:
-            # init (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
-    return ops_count
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+    for i in range(2, n+1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + (i // j))
+    return dp[n] if dp[n] != float('inf') else 0

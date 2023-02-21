d86a8614f9d5d5ba1c7a142c8926dffda969e38f
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-21 17:24:24 +0300

    Make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,14 +4,32 @@ dp[i] represents fewest coins needed to make i
 dp[i-c]+1 represents the number of coins needed to make up the
 remaining amount plus the current coin'''
 
+# def makeChange(coins, total):
+#     # base case
+#     if total <= 0:
+#         return 0
+#     dp = [float('inf')] * (total + 1)  # convert infinity to floating number
+#     dp[0] = 0
+#     for c in coins:
+#         for i in range(c, total + 1):
+#             dp[i] = min(dp[i], dp[i-c]+1)
+#     return dp[total] if dp[total] < float('inf') else -1
+
+'''To make the solution even more efficient,
+we can optimize the space complexity by using a one-dimensional array
+dp instead of a two-dimensional one.
+Instead of storing the fewest number of coins needed for each amount `i`
+up to total for each coin, we can store the fewest number of coins
+needed for each amount i upto total considering all the coins
+we have processed so far.'''
+
 
 def makeChange(coins, total):
-    # base case
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)  # convert infinity to floating number
+    dp = [float('inf')] * (total+1)
     dp[0] = 0
     for c in coins:
-        for i in range(c, total + 1):
+        for i in range(c, total+1):
             dp[i] = min(dp[i], dp[i-c]+1)
     return dp[total] if dp[total] < float('inf') else -1

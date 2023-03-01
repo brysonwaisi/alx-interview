eedcc9b0c4e495d34ea5582438bac52dcf99bd3d
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-01 12:39:13 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,21 @@ def island_perimeter(grid):
     visit = set()
 
     # helper function dfs
-
-    def dfs(i, j):
-        if i >= len(grid) or j >= len(grid[0]) or \
-                i < 0 or j < 0 or grid[i][j] == 0:
+    def dfs(i, j, visit):
+        if (i, j) in visit or grid[i][j] == 0:
             return 1
-
-        if (i, j) in visit:
-            return 0
         visit.add((i, j))
-        perimeter = dfs(i, j + 1)
-        perimeter += dfs(i + 1, j)
-        perimeter += dfs(i, j - 1)
-        perimeter += dfs(i - 1, j)
+        perimeter = dfs(i, j + 1, visit)
+        perimeter += dfs(i + 1, j, visit)
+        perimeter += dfs(i, j - 1, visit)
+        perimeter += dfs(i - 1, j, visit)
         return perimeter
 
+    # iterate over the grid and find the first land cell to start the DFS
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j]:
-                return dfs(i, j)
+            if grid[i][j] == 1:
+                return dfs(i, j, visit)
+
+    # return 0 if no land cells are found
+    return 0

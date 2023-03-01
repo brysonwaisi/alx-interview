eccbdb55992d2ee44eacc105dfde2abd6f1bc083
brysonwaisi <brysonnyamwange@gmail.com>
2023-03-01 12:44:05 +0300

    Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,25 +3,22 @@
 
 
 def island_perimeter(grid):
-    # initialize a set data structure
-    visit = set()
-
-    # helper function dfs
-    def dfs(i, j, visit):
-        if (i, j) in visit or grid[i][j] == 0:
-            return 1
-        visit.add((i, j))
-        perimeter = dfs(i, j + 1, visit)
-        perimeter += dfs(i + 1, j, visit)
-        perimeter += dfs(i, j - 1, visit)
-        perimeter += dfs(i - 1, j, visit)
-        return perimeter
-
-    # iterate over the grid and find the first land cell to start the DFS
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                return dfs(i, j, visit)
-
-    # return 0 if no land cells are found
-    return 0
+    """Computes the perimeter of an island with no lakes.
+    """
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
--- 0x09-island_perimeter/README.md
+++ 0x09-island_perimeter/README.md
@@ -1,18 +1,3 @@
 # Island Perimeter
 We count the perimeter of the island. 1s represent Island and 0s represent water.
 We count the parts connected to the boundary with water
-This is a graph problem that we will solve using DFS.
-
-### DFS base cases
-In the dfs functionality, we pass in coordinates of cells we are visiting and check the following base cases:
-+ if we are out of bounds `i >= len(grid)` if `i` is greater or equal to the num of rows.
-+ if `j` is equal or greater than num of columns `j >= len(grid[0])`
-+ if `i < 0`
-+ if `j < 0`
-+ if position we reached is inbound `grid[i][j] == 0`
-+ if position we reach is in visited set `if (i, j) in visit`
-
-We then start adding to the perimeter on all 4 directions recursively.
-
-Time Complexity: O(n * m)
-Space Complexity: O(n * m)
